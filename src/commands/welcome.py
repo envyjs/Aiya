1c1305fb530ac4251f173a5c70e35f3ecba32ab5
OwnedByWuigi <120682622+OwnedByWuigi@users.noreply.github.com>
2024-05-20 21:38:03 +0100

    Update welcome.py

--- src/commands/welcome.py
+++ src/commands/welcome.py
@@ -1,1 +1,1 @@
-await ctx.send("Welcome to Envy Nexus. To set me up, read the documentation over at GitHub!")
+await ctx.send("Welcome to Envy Nexus. To set me up, read the documentation over at my bio!")
